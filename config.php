9ffbb8b2df35abe4d3502418676c103b167bb282
Web3 Developer <wonderfulplayer@outlook.com>
2023-07-12 10:05:07 +0800

    Upload ads codes

---
+++ config.php
@@ -0,0 +1,17 @@
+<?php
+
+// MySQL Hostname
+$sql_db_host = "localhost";
+// MySQL Database User
+$sql_db_user = "test01";
+// MySQL Database Password
+$sql_db_pass = "Test@123";
+// MySQL Database Name
+$sql_db_name = "test01";
+
+// Site URL
+$site_url = "https://ispotlights.com/test"; // e.g (http://example.com)
+
+// Purchase code
+$purchase_code = "c62bd942-4a99-48db-be4f-f961aff52295"; // Your purchase code, don't give it to anyone. 
+?>
